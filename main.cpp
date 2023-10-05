3454816abe1eb229384c059546f311070507a1a0
Faisallm <faisallawan1997@gmail.com>
2023-10-05 14:46:39 -0700

    printing out to console and variables

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
+    // printing out to console and variables
+
+
     // declaration and assignment of variable
     string characterName = "Faisal";
     // declaration of variable
