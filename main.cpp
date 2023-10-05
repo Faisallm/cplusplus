32dda6316ac4440cb3ca79ef7811b2c1cd2ad0c3
Faisallm <faisallawan1997@gmail.com>
2023-10-05 14:45:10 -0700

    My first Adventure with C++

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // declaration and assignment of variable
+    string characterName = "Faisal";
+    // declaration of variable
+    int characterAge;
+    // assignment of variable
+    characterAge = 20;
+
+    string gender = "man";
+
+    string pronoun = "He";
+
+    cout << "There once was a " << gender << " named " << characterName << endl;
+    cout << pronoun << " was " << characterAge << " years old" << endl;
+
+    // halfway though the story we can change the character name
+    characterName = "Abdul"
+    cout << pronoun << " liked the name " << characterName << endl;
+    cout << "But did not like being " << characterAge <<  endl;
+
+    return 0;
+}
