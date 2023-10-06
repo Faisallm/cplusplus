67f84d8917a4e5a4679a4e6d74e40d80f6b91c8f
Faisallm <faisallawan1997@gmail.com>
2023-10-06 09:53:16 -0700

    Power function

--- main.cpp
+++ main.cpp
@@ -3,31 +3,20 @@
 
 using namespace std;
 
+int power(int baseNum, int powNum) {
+    int result = 1;
+
+    for(int i = 0; i < powNum; i++) {
+        result *= baseNum;
+    }
+
+    return result;
+}
+
 int main()
 {
 
-    // for loops
-    // no cap but c++ is alot like Javascript.
-
-    //int index = 1;
-    //while(index <= 5) {
-      //  cout << index << endl;
-        //index++;
-    //}
-    // array of integers
-    int nums[] = {1, 2, 5, 7, 3};
-    int lengthArr = sizeof(nums) / sizeof(nums[0]);
-
-
-    // iterating through the contents of an array
-    // technically, that's how we roll
-
-    for(int i=0; i < lengthArr; i++) {
-        cout << nums[i] << endl;
-    }
-
-    cout << "The length of the array above is: " << lengthArr << endl;
-
+    cout << power(4, 2) << endl;
 
     return 0;
 }
