4b5a2131c3e37a0195eae44852cd32e11c7add58
Faisallm <faisallawan1997@gmail.com>
2023-10-06 13:05:06 -0700

    Pointers

--- main.cpp
+++ main.cpp
@@ -6,13 +6,28 @@ using namespace std;
 
 int main()
 {
-    // comment, single-line comment
-    /*
-    This is a
-    multi-line
-    comment.
-    */
-    cout << "Comments are fun";
+    int age = 19;
+    // gives us the pointer where a variable is stored.
+    int *pAge = &age;
+    double gpa = 2.7;
+    double *pGpa = &gpa;
+    string name = "Faisal";
+    // storing a pointer for the name
+    // the memory address of name variable
+    string *pName = &name;
+
+    // a pointer is a type of information we can work with in our program.
+    //cout << &age << endl;
+    //cout << &gpa << endl;
+    //cout << &name << endl;
+    cout << pName << endl;
+    cout << *pAge  << endl;
+
+    cout << *&*&gpa;
+
+    // derefferencing a pointer.
+
+
 
     return 0;
 }
