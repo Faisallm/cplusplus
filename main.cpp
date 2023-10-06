6e1596cd7f513964e0663489a2580b54ca59354a
Faisallm <faisallawan1997@gmail.com>
2023-10-06 07:10:05 -0700

    Switch Statements

--- main.cpp
+++ main.cpp
@@ -3,39 +3,45 @@
 
 using namespace std;
 
+string getDayOfWeek(int dayNum) {
+    string dayName;
+
+    switch(dayNum) {
+    case 0:
+        dayName = "Sunday";
+        break;
+    case 1:
+        dayName = "Monday";
+        break;
+    case 2:
+        dayName = "Tuesday";
+        break;
+    case 3:
+        dayName = "Wednesday";
+        break;
+    case 4:
+        dayName = "Thursday";
+        break;
+    case 5:
+        dayName = "Friday";
+        break;
+    case 6:
+        dayName = "Saturday";
+        break;
+    default:
+        dayName = "Invalid day number";
+    }
+
+
+    return dayName;
+}
 
 int main()
 {
 
-    int num1, num2;
-    // this will store a single character value
-    char op;
+    // switch statement is a special type of if statement.
 
-    cout << "Enter first number: ";
-    cin >> num1;
-
-    cout << "Enter operator: ";
-    cin >> op;
-
-    cout << "Enter the second number: ";
-    cin >> num2;
-
-    int result;
-
-    if (op == '+') {
-        result = num1 + num2;
-    } else if (op == '-') {
-        result = num1 - num2;
-    } else if (op == '*') {
-        result = num1 * num2;
-    } else if (op == '/') {
-        result = num1 / num2;
-    } else {
-        cout << "Invalid Operator" << endl;
-    }
-
-    cout << result << endl;
-
+    cout << getDayOfWeek(9);
     return 0;
 }
 
