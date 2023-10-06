04d709eda8b3434200336f4b3140cd429090a37d
Faisallm <faisallawan1997@gmail.com>
2023-10-05 23:03:10 -0700

    Working with numbers

--- main.cpp
+++ main.cpp
@@ -1,24 +1,44 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    string phrase = "Giraffe Academy";
-    string phraseSub;
-    // in python, you can't modify strings
-    // in c++ you can modify strings
-    cout << phrase;
-    cout << phrase.length() << endl;
-    cout << phrase[0] << endl;
-    // I want to change the value of the string
-    phrase[0] = 'B';
-    cout << phrase << endl;
-    cout << phrase.find("Academy", 0) << endl;
-    cout << phrase.find("ffe", 0) << endl;
-    // string splitting in c++
-    phraseSub = phrase.substr(8, 3);
-    cout << phraseSub;
+    // working with numbers
+
+    // to deal with decimal numbers, we have floats and double
+    // double carries more precision compared to floats
+
+    int wnum = 5;
+    double dnum = 5.5;
+
+    wnum++;
+    dnum--;
+
+    wnum += 30;
+    dnum -= 45;
+
+    // printout 6
+    cout << wnum << endl;
+    cout << dnum << endl;
+
+    // how do int and doubles work together
+
+    // we are going to get a decimal number back
+    cout << typeid(5.5 + 9).name() << endl;
+    cout << 10 / 3.0 << endl;
+
+    cout << pow(2, 5) << endl;
+    cout << sqrt(36) << endl;
+    // round down
+    cout << round(4.3) << endl;
+    // round up
+    cout << ceil(4.3) << endl;
+    // round down
+    cout << floor(4.8) << endl;
+    cout << fmax(34.5, 34.5) << endl;
+
 
     return 0;
 }
