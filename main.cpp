37894e6b1029f408e3d580655c7c0ecd79954955
Faisallm <faisallawan1997@gmail.com>
2023-10-05 20:10:58 -0700

    CPP datatypes

--- main.cpp
+++ main.cpp
@@ -4,27 +4,37 @@ using namespace std;
 
 int main()
 {
-    // printing out to console and variables
+    // data-types
+
+    // char (short for character), used to store a single character
+    // store one single character
+    // it uses a single quotation mark
+
+    char grade = 'A';
+    // to store multiple character's we use a string
+    string phrase = "Girrafe Academy";
+
+    // numbers (two types, whole numbers and decimal numbers)
+    // decimal numbers can also be called floating point numbers
+
+    // whole numbers
+    int age = 50;
+    int temperature = -5;
+    // decimal numbers
+    double gpa = 3.99;
+
+    // boolean (true false value)
+
+    bool isMale = true;
+
+    cout << "age: " << 50 << endl;
 
 
-    // declaration and assignment of variable
-    string characterName = "Faisal";
-    // declaration of variable
-    int characterAge;
-    // assignment of variable
-    characterAge = 20;
+    cout << grade << endl;
+    cout << phrase << endl;
+    cout << gpa << endl;
+    cout << isMale << endl;
 
-    string gender = "man";
-
-    string pronoun = "He";
-
-    cout << "There once was a " << gender << " named " << characterName << endl;
-    cout << pronoun << " was " << characterAge << " years old" << endl;
-
-    // halfway though the story we can change the character name
-    characterName = "Abdul"
-    cout << pronoun << " liked the name " << characterName << endl;
-    cout << "But did not like being " << characterAge <<  endl;
 
     return 0;
 }
