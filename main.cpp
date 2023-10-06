e4dda1c705d6257d56606b3e8a7082500e9225ae
Faisallm <faisallawan1997@gmail.com>
2023-10-06 01:15:14 -0700

    Getting input from users

--- main.cpp
+++ main.cpp
@@ -5,39 +5,21 @@ using namespace std;
 
 int main()
 {
-    // working with numbers
+    //char age;
+    //double ager;
 
-    // to deal with decimal numbers, we have floats and double
-    // double carries more precision compared to floats
+    //cout << "Enter your name: ";
+    //cin >> age;
 
-    int wnum = 5;
-    double dnum = 5.5;
+    //cout << "You are " << age << " years old" << endl;
+    //cout << "You are " << ager << " years old" << endl;
 
-    wnum++;
-    dnum--;
+    string name;
 
-    wnum += 30;
-    dnum -= 45;
+    cout << "Enter your name: ";
+    getline(cin, name);
 
-    // printout 6
-    cout << wnum << endl;
-    cout << dnum << endl;
-
-    // how do int and doubles work together
-
-    // we are going to get a decimal number back
-    cout << typeid(5.5 + 9).name() << endl;
-    cout << 10 / 3.0 << endl;
-
-    cout << pow(2, 5) << endl;
-    cout << sqrt(36) << endl;
-    // round down
-    cout << round(4.3) << endl;
-    // round up
-    cout << ceil(4.3) << endl;
-    // round down
-    cout << floor(4.8) << endl;
-    cout << fmax(34.5, 34.5) << endl;
+    cout << "Hello " << name;
 
 
     return 0;
