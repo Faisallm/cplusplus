91168a900a6b28a69e93b94beef5ee9df0dee24c
Faisallm <faisallawan1997@gmail.com>
2023-10-06 10:19:17 -0700

    comments

--- main.cpp
+++ main.cpp
@@ -6,21 +6,13 @@ using namespace std;
 
 int main()
 {
-    int numberGrid[3][2] = {
-        {1, 2},
-        {3, 4},
-        {5, 6}
-    };
-
-
-    // looping through the content of the 2D array.
-    // these are known as nested loops
-    for(int i = 0; i < 3; i++) {
-        for (int j = 0; j < 2; j++) {
-            cout << numberGrid[i][j];
-        }
-        cout << endl;
-    }
+    // comment, single-line comment
+    /*
+    This is a
+    multi-line
+    comment.
+    */
+    cout << "Comments are fun";
 
     return 0;
 }
