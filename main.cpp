f080864d3875f30522f28a4df3a9b2d95a1832bc
Faisallm <faisallawan1997@gmail.com>
2023-10-06 06:09:37 -0700

    Functions with arguments

--- main.cpp
+++ main.cpp
@@ -3,25 +3,16 @@
 
 using namespace std;
 
-// declare the function, give it a signature.
-// function signature / function stub
-void sayHi(string name, int age);
+double cube(double num) {
+    // using the math function.
+    // return signifies the end of the function
+    return pow(num, 3);
+}
 
 int main()
 {
-    // functions
-
-    cout << "Top" << endl;
-    // running the function
-    sayHi("Faisal", 20);
-    sayHi("Amina", 15);
-    cout << "Bottom";
+    cout << cube(2.0);
 
     return 0;
 }
 
-
-// void means that the function will not return anything.
-void sayHi(string name, int age) {
-    cout << "Hello "<< name << " you are " << age << " years old." << endl;
-}
