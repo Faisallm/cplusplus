5224a47199fddcf387efa7d328d3acc7bdbdfb0e
Faisallm <faisallawan1997@gmail.com>
2023-10-06 10:14:40 -0700

    2D arrays and Nested for loops

--- main.cpp
+++ main.cpp
@@ -6,19 +6,21 @@ using namespace std;
 
 int main()
 {
-
-    // 2-dimensional arrays and nested for loops
-
-    // 3 ROWS AND 2 COLUMNS,
-    // essentially we are building a matrix
     int numberGrid[3][2] = {
         {1, 2},
         {3, 4},
         {5, 6}
     };
 
-    // printing the 2D arrays
-    cout << numberGrid[2][1];
+
+    // looping through the content of the 2D array.
+    // these are known as nested loops
+    for(int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            cout << numberGrid[i][j];
+        }
+        cout << endl;
+    }
 
     return 0;
 }
