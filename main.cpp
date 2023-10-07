1582b2890fb52baabdb66487a67e2eee422c456d
Faisallm <faisallawan1997@gmail.com>
2023-10-06 17:21:32 -0700

    Constructors

--- main.cpp
+++ main.cpp
@@ -13,6 +13,24 @@ class Book {
         string title;
         string author;
         int pages;
+        // a constructor is a special object that will be called...
+        // whenever we create a book object
+
+        //default values
+        // we can define multiple constructors
+        // we are giving the user multiple ways of...
+        // creating an object.
+        Book () {
+            title = "no title";
+            author = "no author";
+            pages = 0;
+        }
+
+        Book(string aTitle, string aAuthor, int aPages) {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
 
 };
 
@@ -21,19 +39,13 @@ int main()
 {
 
     // A class is essentially a new data-type
-    Book book1;
+    // objects are instances of a class
+    // using constructor functions makes class declarations easier.
+    Book book1("Harry Potter", "JK Rowling", 500);
 
-    book1.title = "Harry Potter";
-    book1.author = "JK Rowling";
-    book1.pages = 500;
+    Book book2("Lord of the Rings", "Tolkein", 700);
 
-    Book book2;
-
-    book2.title = "Lord of the Rings";
-    book2.author = "Tolkein";
-    book2.pages = 700;
-    book2.title = "Hunger games";
-
+    Book book3;
 
     cout << book1.title << endl;
     cout << book1.pages << endl;
@@ -41,6 +53,10 @@ int main()
     cout << book2.title << endl;
     cout << book2.pages << endl;
     cout << book2.author << endl;
+
+    cout << book3.title;
+    cout << book3.author;
+    cout << book3.pages;
 
 
     return 0;
